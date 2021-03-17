42e6b685e2888045a14db1c2c4fa5ca24c8c6186
natepage <nathan.page86@gmail.com>
2021-03-17 11:27:20 +1100

    [Monorepo] use grep to remove config header

---
+++ src/Generators/Exceptions/ValidationFailedException.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyBankFiles\Generators\Exceptions;
+
+use EonX\EasyErrorHandler\Exceptions\ValidationException;
+use Throwable;
+
+final class ValidationFailedException extends ValidationException
+{
+    /**
+     * ValidationFailedException constructor.
+     *
+     * @param mixed[] $errors
+     */
+    public function __construct(array $errors, ?string $message = null, ?int $code = null, ?Throwable $previous = null)
+    {
+        $message = \sprintf('%s. %s', $message ?? '', $this->getErrorsToString($errors));
+
+        $this->setErrors($errors);
+
+        parent::__construct($message, $code ?? 0, $previous);
+    }
+
+    /**
+     * Get validation errors as string representation.
+     *
+     * @param mixed[]|null $errors
+     */
+    public function getErrorsToString(?array $errors = null): string
+    {
+        $pattern = '[attribute => %s, value => %s, rule => %s]';
+        $errorsToString = '';
+
+        foreach ($errors ?? $this->getErrors() as $error) {
+            $errorsToString .= \sprintf($pattern, $error['attribute'], $error['value'], $error['rule']);
+        }
+
+        return $errorsToString;
+    }
+}
---
+++ tests/Parsers/Ack/Results/PaymentAcknowledgementTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyBankFiles\Tests\Parsers\Ack\Results;
+
+use DateTime;
+use EonX\EasyBankFiles\Parsers\Ack\Results\PaymentAcknowledgement;
+use EonX\EasyBankFiles\Tests\Parsers\TestCase;
+
+final class PaymentAcknowledgementTest extends TestCase
+{
+    /**
+     * Should return datetime as DateTime object.
+     *
+     * @group Ack-PaymentAcknowledgement
+     */
+    public function testShouldReturnDateTimeAsObject(): void
+    {
+        $dateString = '2017/10/17';
+        $expected = [
+            '@value' => new DateTime($dateString),
+        ];
+
+        $acknowledgement = new PaymentAcknowledgement([
+            'dateTime' => [
+                '@value' => $dateString,
+            ],
+        ]);
+
+        self::assertEquals($expected, $acknowledgement->getDateTime());
+    }
+}
